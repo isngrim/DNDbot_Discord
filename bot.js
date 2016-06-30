bc6a07678c39024c021f3e1fa3509436fc2c53f2
mynameis7 <andrew.rather5@gmail.com>
2016-06-29 23:02:59 -0400

    FIX PLZ

--- bot.js
+++ bot.js
@@ -35,26 +35,18 @@ bot.on('message', function(user, userID, channelID, message, event) {
     console.log(typeof(message));
     console.log(message);
     if (message.startsWith("!roll") ){
-        try {
-            var rolls = roll(message);
-            for(var i = 0; i < rolls.length; i++) {
-                var aggregate = "";
-                for(var j = 0; j < 10; j++) {
-                    if(10*i + j >= rolls.length)
-                        break;
-                    aggregate += rolls[i*10 + j] + "\n";
-                }
-                bot.sendMessage({
-                    to: channelID,
-                    message: aggregate
-                });
+        var rolls = roll(message);
+        for(var i = 0; i < rolls.length; i++) {
+            var aggregate = "";
+            for(var j = 0; j < 10; j++) {
+                if(10*i + j >= rolls.length)
+                    break;
+                aggregate += rolls[i*10 + j] + "\n";
             }
-        }
-        catch(err) {
             bot.sendMessage({
                 to: channelID,
-                message: "Error, could not roll"
-            })
+                message: aggregate
+            });
         }
     }
 });
